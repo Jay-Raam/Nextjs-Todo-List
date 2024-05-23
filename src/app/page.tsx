9a8f5be46616c7d75f683d8e232ef858f6b3b010
Jayasriraam-s <jayasriraam27@gmail.com>
2024-05-23 17:16:36 +0530

    second code change commit

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -5,7 +5,7 @@ export default function Home() {
   return (
     <section>
       <div className="flex justify-around items-center gap-[50px] w-full mt-[14px] p-2">
-        <h1 className="text-2xl">Day's Task</h1>
+        <h1 className="text-2xl">Day&apos;s Task</h1>
         <Dark />
       </div>
       <div>
